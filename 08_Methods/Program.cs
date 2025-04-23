c2f458284607bbd771220fb600c7222fe42f3263
Yavuz Alp Beydogan <alpixel@outlook.com>
2025-04-23 23:21:11 +0300

    güncelleme ve yeni konu

---
+++ 08_Methods/Program.cs
@@ -0,0 +1,44 @@
+﻿#region Void Metodlar
+// void customerName(){
+//     Console.WriteLine("Ahmet");
+//     Console.WriteLine("mehmet");
+//     Console.WriteLine("yavuz");
+// }
+// customerName();
+
+// void sum(){
+//     int x = 1;
+//     int y=2;
+//     int z=x+y;
+//     Console.WriteLine(z);
+// }
+// sum();
+#endregion
+
+#region Geriye Değer Döndürmeyen Parametreli Metodlar
+// void customerCard(string name, string lastName){
+//     Console.WriteLine($"Müşteri: {name} {lastName}");
+// }
+// customerCard("yavuz","alp");
+// customerCard("ayse","nur");
+
+// void sum(int num1, int num2, int num3){
+//     int result = num1 +num2 +num3;
+//     Console.WriteLine(result);
+// }
+// sum(2,4,6);
+#endregion
+
+
+
+
+
+
+
+
+
+
+
+
+
+
