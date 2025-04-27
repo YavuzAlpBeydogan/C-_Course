e2870c305f476dd1959a7a3f67b29451c8ded411
Yavuz Alp Beydogan <alpixel@outlook.com>
2025-04-27 13:01:36 +0300

    new week

--- 08_Methods/Program.cs
+++ 08_Methods/Program.cs
@@ -29,10 +29,48 @@
 // sum(2,4,6);
 #endregion
 
+#region Geriye Değer Döndüren Metodlar
+// string customerCard(){
+//     string name = "Yavuz";
+//     string surname ="Beydogan";
+//     return name + " "+ surname;
+// }
+// Console.WriteLine(customerCard());
+#endregion
 
+#region Geriye Değer Döndüren Parametreli Metodlar
+// string CountryCard(string name, string capital, string flag){
+//     string cardInfo="-Ülke:" + name+ " -Başkent: "+ capital+ " -Bayrak Rengi: " +flag;
+//     return cardInfo;
+// }
+// string x, y ,z;
+// Console.Write("ÜLkeyi Giriniz: ");
+// x=Console.ReadLine();
 
+// Console.Write("Başkenti Giriniz: ");
+// y=Console.ReadLine();
 
+// Console.Write("Bayrak Rengini Giriniz: ");
+// z=Console.ReadLine();
 
+// Console.WriteLine(CountryCard(x,y,z));
+
+#endregion
+
+#region Geriye Değer Döndüren İnt Metodlar
+// int sum(int num1, int num2){
+//     int result=num1+num2;
+//     return result;
+// }
+// int x , y ;
+// Console.Write("1.Sayıyı Giriniz: ");
+// x=int.Parse(Console.ReadLine());
+
+// Console.Write("2.Sayıyı Giriniz: ");
+// y=int.Parse(Console.ReadLine());
+
+// Console.WriteLine(x+" + " +y+ " = "+sum(x,y));
+#endregion
 
 
 
